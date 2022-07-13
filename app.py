0d2f608830e4ba4747d01ff35c521898b56d282f
machdd365 <jkk1637@naver.com>
2022-07-13 18:22:36 +0900

    메인페이지 api

--- app.py
+++ app.py
@@ -125,6 +125,11 @@ def get_posts():
 #     except (jwt.ExpiredSignatureError, jwt.exceptions.DecodeError):
 #         return redirect(url_for("home"))
 
+@app.route("/index", methods=["GET"]) #DB에서 pet 받아오기
+def web_pet_get():
+    pet_list = list(db.pet.find({}, {'_id': False}))
+    return jsonify({'pets':pet_list})
+
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
