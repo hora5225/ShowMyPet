dba32f7f2f16818441a2e46d4256e070413fcd5b
hora5225 <hora5225@gmail.com>
2022-07-14 14:45:53 +0900

    [현식]기능구현완료

--- app.py
+++ app.py
@@ -163,6 +163,19 @@ def upload():
 
     return jsonify({'msg': '저장 완료'})
 
+# 파일 정보 가져오는 용-현식
+# @app.route('/detail/<keyword>')
+@app.route('/detail/')
+def detail():
+    #request.args.get 이용해서 url 뒤에 ?word_give=파일명변수  해줘서 받아온 뒤 word_receive에 저장
+    word_receive = request.args.get("word_give")
+    return render_template("detail.html", word=word_receive)
+
+#디비에서 정보 가져오기
+@app.route('/detail_listing', methods=['GET'])
+def detail_listing():
+    pets = list(db.pet.find({}, {'_id': False}))
+    return jsonify({"msg": 'ok', 'all_pet': pets})
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
